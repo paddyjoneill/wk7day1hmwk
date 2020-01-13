4a50310f24d710451484d450200080e36ccbdb7d
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-01-13 15:54:13 +0000

    mvp

--- src/app.js
+++ src/app.js
@@ -2,4 +2,17 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Javascript Loaded');
+  new Vue({
+    el: '#app',
+    data: {
+      todos: ["Buy Shopping", "Wash Car", "Do Laundry"],
+      newTodo: ""
+    },
+    methods: {
+      addTodo: function() {
+        this.todos.push(this.newTodo)
+        this.newTodo = ""
+      }
+    }
+  })
 })
